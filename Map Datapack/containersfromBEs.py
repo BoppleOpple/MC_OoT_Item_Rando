6a5a979ea5c018501f101b54bb7487693828858c
BoppleOpple <BoppleOpple@gmail.com>
2022-02-24 16:29:42 -0500

    forgot to save kek

--- Map Datapack/containersfromBEs.py
+++ Map Datapack/containersfromBEs.py
@@ -5,6 +5,7 @@ with open("blockentitiespruned.json") as allBEsJSON:
   blockEntities = list(filter(lambda object : object["id"] in containers, json.load(allBEsJSON)))
   blockEntities.sort(key=lambda container : container["region"])
 
+
 with open("containers.json", 'w') as JSONFile:
   print(json.dumps(blockEntities))
   JSONFile.write(json.dumps(blockEntities))
