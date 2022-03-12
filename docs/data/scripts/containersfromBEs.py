b5893efa27703d16ded5be9330adb86eac662b57
BoppleOpple <BoppleOpple@gmail.com>
2022-03-12 18:57:27 -0500

    Added missing containers + improved fs

---
+++ docs/data/scripts/containersfromBEs.py
@@ -0,0 +1,17 @@
+import json
+containers = [
+  "universal_minecraft:chest", 
+  "universal_minecraft:barrel", 
+  "universal_minecraft:dispenser", 
+  "universal_minecraft:hopper", 
+  "universal_minecraft:dropper", 
+  "universal_minecraft:trapped_chest"
+]
+blockEntities = []
+with open("blockentitiespruned.json") as allBEsJSON:
+  blockEntities = list(filter(lambda object : object["id"] in containers, json.load(allBEsJSON)))
+  blockEntities.sort(key=lambda container : container["region"])
+
+with open("containers.json", 'w') as JSONFile:
+  print(json.dumps(blockEntities))
+  JSONFile.write(json.dumps(blockEntities))
